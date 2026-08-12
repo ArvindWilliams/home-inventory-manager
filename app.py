874f63703be906b6227336d930e5184ccac1db59
Arvind <n12810029@qut.edu.au>
2026-08-12 14:18:40 +1000

    HIM-16 Add login template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -17,3 +17,7 @@ def home():
 
 with app.app_context():
     db.create_all()
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
