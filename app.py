81944f0390bc779805536ec310897b5f728bf094
Arvind <n12810029@qut.edu.au>
2026-08-12 13:52:28 +1000

    HIM-15 Add User database model

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///him.db"
+
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
 
 @app.route("/")
 def home():
     return "Home Inventory Manager"
+
+with app.app_context():
+    db.create_all()
